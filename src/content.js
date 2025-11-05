5132c051e3acb43157c167a9356fb1f8217b22ff
itijuku <kanjukuitijuku@gmail.com>
2025-11-05 15:30:17 +0900

    Update content.js

--- src/content.js
+++ src/content.js
@@ -37,37 +37,99 @@ function check(_url){
         .then(data=>getAnswer(data))
         
         function getAnswer(data){
-            const localData = data["questions"][nowQIndex]["choices"];
-            const arrayData = Object.entries(localData);
-            let answerId = "";
-            for(const a of arrayData){
-                if(a[1]["correct"]){
-                    answerId = a[1]["id"];
-                    break;
-                }
+            let type = "";
+            if(Array.isArray(data["questions"])){
+                console.log(nowQIndex)
+                type = data["questions"][0]["answer_type"];
+            }else{
+                type = data["questions"]["answer_type"];
             }
 
-            let button = document.querySelector(`button[value="${answerId}"]`);
-            const checkInterval = setInterval(function(){
-                button = document.querySelector(`button[value="${answerId}"]`);
-                if(button){
-                    clearInterval(checkInterval);
-                    console.log("get button");
-                    button.click();
+            console.log(type)
+            if(type !== "grouped_choices"){
+                const localData = data["questions"][nowQIndex]["choices"];
+                const arrayData = Object.entries(localData);
+                let answerId = "";
+                for(const a of arrayData){
+                    if(a[1]["correct"]){
+                        answerId = a[1]["id"];
+                        break;
+                    }
+                }
 
-                    setTimeout(function(){
-                        const sendButton = document.querySelector(".RaisedButton--BU1aR.AnswerChoices__Button--qXmrH.btn.btn-primary--p7JPl")
-                    
-                        sendButton.click();
+                let button = document.querySelector(`button[value="${answerId}"]`);
+                const checkInterval = setInterval(function(){
+                    button = document.querySelector(`button[value="${answerId}"]`);
+                    if(button){
+                        clearInterval(checkInterval);
+                        console.log("get button");
+                        button.click();
 
                         setTimeout(function(){
-                            const nextButton = document.querySelector(".RaisedButton--BU1aR.AnswerChoices__Button--qXmrH.btn.btn-primary--p7JPl")
+                            const sendButton = document.querySelector(".RaisedButton--BU1aR.AnswerChoices__Button--qXmrH.btn.btn-primary--p7JPl")
                         
-                            nextButton.click();
-                        },25)
-                    },25)
+                            sendButton.click();
+
+                            setTimeout(function(){
+                                const nextButton = document.querySelector(".RaisedButton--BU1aR.AnswerChoices__Button--qXmrH.btn.btn-primary--p7JPl")
+                            
+                                nextButton.click();
+                            },35)
+                        },35)
+                    }
+                },25)
+            }else{
+                const localData = data["questions"];
+                let answerId = [];
+                for(const lq of localData[nowQIndex]["choices"]){
+                    if(lq["correct"]){
+                        answerId.push(lq["id"]);
+                        console.log(lq["id"],lq["body"][0]["text"]);
+                    }
                 }
-            },20)
+
+                let ultag = document.querySelector(".QuizChoiceList--iW4vr");
+
+                const getUltag = setInterval(function(){
+                    ultag = document.querySelector(".QuizChoiceList--iW4vr");
+
+                    if(ultag){
+                        clearInterval(getUltag);
+                        ultag = ultag.querySelectorAll("li");
+                        get();
+                        async function get() {
+                            for(const li of ultag){
+                                console.log(li)
+                                const button1 = li.querySelector("button");
+                                button1.click();
+                                await new Promise(resolve => setTimeout(resolve, 20));
+                                const wrapper = document.querySelector(".QuizDropdown__Menu--EPeAX.dropdown-menu--N1V_1.show--QuX7k");
+                                const buttons = wrapper.querySelectorAll("button");
+                                for(const b of buttons){
+                                    if(answerId.includes(b.value)){
+                                        b.click();
+                                        break;
+                                    }
+                                }
+                            }
+
+                            const sendButton = document.querySelector(".RaisedButton--BU1aR.AnswerChoices__Button--qXmrH.btn.btn-primary--p7JPl")                        
+                            sendButton.click();
+                            await new Promise(resolve => setTimeout(resolve, 50));
+
+                            const nextButton = document.querySelector(".RaisedButton--BU1aR.AnswerChoices__Button--qXmrH.btn.btn-primary--p7JPl")
+                            nextButton.click();
+
+                            await new Promise(resolve => setTimeout(resolve, 50));
+
+                            const nextButton2 = document.querySelector(".RaisedButton--BU1aR.AnswerChoices__Button--qXmrH.btn.btn-primary--p7JPl")
+                            nextButton2.click();
+
+                            await new Promise(resolve => setTimeout(resolve, 25));
+                        }
+                    }
+                })
+            }
         }
     }
 }
